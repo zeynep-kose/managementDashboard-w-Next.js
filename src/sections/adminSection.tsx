0ac23388914ad1d1b801542b68fc1a668c723870
Zeynep Kose <zynpk1903@gmail.com>
2024-12-13 15:15:17 +0300

    updates on adminPage

--- src/sections/adminSection.tsx
+++ src/sections/adminSection.tsx
@@ -1,25 +1,37 @@
+import AttendanceChart from "@/components/AttendanceChart";
+import CountChart from "../components/CountChart";
 import UserCard from "../components/UseCard";
 import React from "react";
-
-type Props = {};
+import FinanceChart from "@/components/FinanceChart";
 
 const AdminSection = () => {
   return (
-    <div className="flex p-4 gap-4 flex-row sm:flex-col">
-      <div className="w-full lg:w-2/3 ">
-        {/* {[...Array(4)].map((_, index) => (
-          <div key={index}>
-            <UserCard />
-          </div>
-        ))} */}
+    <div className="p-4 flex gap-4 flex-row sm:flex-col h-full">
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
         <div className="flex gap-4 justify-between flex-wrap">
           <UserCard type="student" />
           <UserCard type="teacher" />
           <UserCard type="parent" />
           <UserCard type="staff" />
         </div>
+
+        <div className="flex gap-4 flex-col lg:flex-row">
+          <div className="flex">
+            <div className="w-full lg:w-1/3 h-[450px]">
+              <CountChart />
+            </div>
+            <div className="w-full lg:w-2/3  h-[450px]">
+              <AttendanceChart />
+            </div>
+          </div>
+        </div>
+
+        <div className="w-full h-[500px]">
+          <FinanceChart />
+        </div>
       </div>
-      <div className="w-full lg:w-1/3">right</div>
+      {/* RIGHT */}
+      <div className="w-full lg:w-1/3 flex flex-col gap-8"></div>
     </div>
   );
 };
