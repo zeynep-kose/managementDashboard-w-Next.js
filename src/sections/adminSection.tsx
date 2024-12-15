9d0c83e269aac99fea04b695c370452b489432cc
Zeynep Kose <zynpk1903@gmail.com>
2024-12-15 16:43:20 +0300

    calender was added(right Part)

--- src/sections/adminSection.tsx
+++ src/sections/adminSection.tsx
@@ -3,11 +3,12 @@ import CountChart from "../components/CountChart";
 import UserCard from "../components/UseCard";
 import React from "react";
 import FinanceChart from "@/components/FinanceChart";
+import EventCalendar from "@/components/EventCalendar";
 
 const AdminSection = () => {
   return (
     <div className="p-4 flex gap-4 flex-row sm:flex-col h-full">
-      <div className="w-full lg:w-2/3 flex flex-col gap-8">
+      <div className="w-full lg:w-3/5 flex flex-col gap-4">
         <div className="flex gap-4 justify-between flex-wrap">
           <UserCard type="student" />
           <UserCard type="teacher" />
@@ -31,7 +32,9 @@ const AdminSection = () => {
         </div>
       </div>
       {/* RIGHT */}
-      <div className="w-full lg:w-1/3 flex flex-col gap-8"></div>
+      <div className="w-full lg:w-2/5 flex flex-col gap-8">
+        <EventCalendar />
+      </div>
     </div>
   );
 };
